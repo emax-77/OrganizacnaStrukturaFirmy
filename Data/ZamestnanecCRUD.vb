853065aeaa2a9d06ab7ed4d4916b3ea052bd86b0
emax-77 <peter.wirth@gmail.com>
2026-03-02 06:09:45 +0100

    Vymazanie nepouzitej metody

--- Data/ZamestnanecCRUD.vb
+++ Data/ZamestnanecCRUD.vb
@@ -64,36 +64,6 @@ Public Class ZamestnanecCRUD
         Return Nothing
     End Function
 
-    Public Function ZiskajZamestnancovPodlaZaradenia(zaradenie As String, zaradenieId As Integer) As List(Of mZamestnanec)
-        Dim vysledok As New List(Of mZamestnanec)
-        Using spojenie = VytvorSpojenie()
-            Dim sql = "SELECT Id, Titul, Meno, Priezvisko, Telefon, Email, Zaradenie, ZaradenieId, UzolTyp, UzolId FROM Zamestnanec WHERE Zaradenie=@Zaradenie AND ZaradenieId=@ZaradenieId"
-            Using prikaz = New SqlCommand(sql, spojenie)
-                PridajParameter(prikaz, "@Zaradenie", zaradenie)
-                PridajParameter(prikaz, "@ZaradenieId", zaradenieId)
-                spojenie.Open()
-                Using reader = prikaz.ExecuteReader()
-                    While reader.Read()
-                        vysledok.Add(New mZamestnanec With {
-                            .Id = reader.GetInt32(0),
-                            .Titul = If(reader.IsDBNull(1), Nothing, reader.GetString(1)),
-                            .Meno = reader.GetString(2),
-                            .Priezvisko = reader.GetString(3),
-                            .Telefon = If(reader.IsDBNull(4), Nothing, reader.GetString(4)),
-                            .Email = If(reader.IsDBNull(5), Nothing, reader.GetString(5)),
-                            .Zaradenie = reader.GetString(6),
-                            .ZaradenieId = reader.GetInt32(7),
-                            .ZaradenieTyp = ParseZaradenieTyp(reader.GetString(6)),
-                            .UzolTyp = ParseTypUzla(reader.GetString(8)).Value,
-                            .UzolId = reader.GetInt32(9)
-                        })
-                    End While
-                End Using
-            End Using
-        End Using
-        Return vysledok
-    End Function
-
     Public Function UlozZamestnanca(zamestnanec As mZamestnanec) As Integer
         Using spojenie = VytvorSpojenie()
             Dim sql = "INSERT INTO Zamestnanec (Titul, Meno, Priezvisko, Telefon, Email, Zaradenie, ZaradenieId, UzolTyp, UzolId) OUTPUT INSERTED.Id VALUES (@Titul, @Meno, @Priezvisko, @Telefon, @Email, @Zaradenie, @ZaradenieId, @UzolTyp, @UzolId)"
